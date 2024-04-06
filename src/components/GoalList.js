bc108defb890bd3671b17e769ac9f55fd661d289
Sagar Jadhav <sagarmj1502@gmail.com>
2024-04-06 12:57:59 -0700

    initial commeant

---
+++ src/components/GoalList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Finish </li>
+      <li>All about</li>
+      <li>students</li>
+    </ul>
+  );
+};
